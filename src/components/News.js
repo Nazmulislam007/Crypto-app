bc7fa0424c675e1412379b475f16c05ff33e24dd
nazmul <nazmulislam.ni897@gmail.com>
2022-05-06 12:15:13 +0600

    crypto news

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,66 @@
+import { Avatar, Card, Col, Row, Select, Typography } from "antd";
 import React from "react";
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import moment from "moment";
 
-const News = () => {
-  return <div>News</div>;
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: "Cryptocurrency",
+    count: simplified ? 6 : 12,
+  });
+  if (!cryptoNews?.value) return "Loading...";
+  const demoImage =
+    "http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg";
+
+  return (
+    <>
+      <Row gutter={[24, 24]}>
+        {cryptoNews.value.map((news, i) => (
+          <Col xs={24} sm={12} lg={8} key={i}>
+            <Card hoverable className="news-card">
+              <a href={news.url} target="_blank" rel="noreferrer">
+                <div className="news-image-container">
+                  <Title className="news-title" level={4}>
+                    {news.name}
+                  </Title>
+                  <img
+                    style={{ maxWidth: "200px", maxHeight: "100px" }}
+                    src={news?.image?.thumbnail?.contentUrl || demoImage}
+                    alt={news.name}
+                  />
+                </div>
+                <p>
+                  {news.description > 100
+                    ? `${news.description.substring(0, 100)}...`
+                    : news.description}
+                </p>
+                <div className="porvider-container">
+                  <div>
+                    <Avatar
+                      src={
+                        news.provider[0]?.image?.thumbnail?.contentUrl ||
+                        demoImage
+                      }
+                      alt="news"
+                    />
+                    <Text className="provider-name">
+                      {news.provider[0]?.name}
+                    </Text>
+                  </div>
+                  <Text>
+                    {moment(news.datePublished).startOf("ss").fromNow()}
+                  </Text>
+                </div>
+              </a>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default News;
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+
+const cryptoNewsHeaders = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+  "X-RapidAPI-Key": "56db643bcfmsh6e62fc78c88b4e8p1e3ebejsn90341ecef83d",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (bulider) => ({
+    getCryptoNews: bulider.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
