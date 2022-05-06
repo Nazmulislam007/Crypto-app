ad3e9f083b14daf2331202a14dd0c9065a45a2f6
nazmul <nazmulislam.ni897@gmail.com>
2022-05-06 06:57:05 +0600

    top 10 cryptocurrencies

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,7 +1,49 @@
-import React from "react";
+import { Card, Col, Row } from "antd";
+import millify from "millify";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
-const Cryptocurrencies = () => {
-  return <div>Cryptocurrencies</div>;
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data, isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState(data?.data?.coins);
+
+  if (isFetching) return "Loading...";
+
+  return (
+    <>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((currency) => (
+          <Col
+            xs={24}
+            sm={12}
+            lg={6}
+            className="crypto-card"
+            key={currency.uuid}
+          >
+            <Link to={`/crypto/${currency.uuid}`}>
+              <Card
+                hoverable
+                title={`${currency.rank}. ${currency.name}`}
+                extra={
+                  <img
+                    className={`crypto-image`}
+                    src={currency.iconUrl}
+                    alt={currency.name}
+                  />
+                }
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Cryptocurrencies;
